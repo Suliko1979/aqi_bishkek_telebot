d8a0490c420f8041c6831d3b8b0f07cebe0124d7
sm2401 <sultan.mamytov@gmail.com>
2021-03-28 20:02:43 +0600

    Additional parameters

--- AQI.py
+++ AQI.py
@@ -1,24 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import lxml
 import telebot
 
 
-token = "1736982389:AAFWK5rYZqHVeEMzDZ94Aw3VJSBX9q2WFLo"
+token = "1736982389:AAHWkX5UZbcFluMGM8MyGF87SA1fx5fpn4A"
+
 bot = telebot.TeleBot(token)
-
-URL = 'https://aqicn.org/city/kyrgyzstan/bishkek/us-embassy/'
-
-response = requests.get(URL, 'lxml')
-soup = BeautifulSoup(response.content, 'lxml')
-items = soup.find('div', {'id': "aqiwgtvalue"})
-text_1 = items.text
-print(text_1)
 
 welcome_text = "Привет! Качество воздуха в Бишкеке сейчас: "
 
+URL = 'https://www.iqair.com/ru/kyrgyzstan/bishkek'
+
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, 'lxml')
+aqi_descritpive = soup.find('span', class_ = 'aqi-status__text').getText()
+aqi_in_numbers = soup.find('p', class_ = 'aqi-value__value').getText()
+
 @bot.message_handler(content_types=['text'])
 def main(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, welcome_text   +   text_1 + " пунктов." + " Чтобы узнать больше, посети сайт https://aqicn.org/map/bishkek/ru/")
+    if message.text.lower() == 'привет' or "ghbdtn":
+        bot.send_message(message.chat.id, welcome_text   +   aqi_in_numbers + " пунктов."
+                          " \n Говоря словами, качество воздуха в категории  "
+                         + aqi_descritpive)
 
 bot.polling()
